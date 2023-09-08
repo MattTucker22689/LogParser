7b6461c575fb65d009f8d42d2b9ebdd500d9faf7
Matt Tucker <mathew.tucker.1@gmail.com>
2023-09-08 11:37:29 -0700

    Add files via upload

---
+++ mainwindow.py
@@ -0,0 +1,154 @@
+# This Python file uses the following encoding: utf-8
+from datetime import datetime
+import sys
+import pandas as pd
+
+#from PyQt5.uic.properties import QtWidgets
+from PySide6.QtWidgets import QApplication, QMainWindow,  QPushButton, QFileDialog, QTableWidgetItem
+
+# Important:
+# You need to run the following command to generate the ui_form.py file
+#     pyside6-uic form.ui -o ui_form.py, or
+#     pyside2-uic form.ui -o ui_form.py
+from ui_parserGUI_3 import Ui_MainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.printAll = False
+        self.printSellected = False
+
+        self.ui.pushButton.clicked.connect(self.tableBuilder)
+
+    def timeDif(self, use_out, use_in):
+        # Find difference and return value
+        delta = use_in - use_out
+        return str(delta)
+
+
+    # To be implemented in future versions
+    def getUser(self):
+        return None
+
+
+    def getStart(self):
+        temp = self.ui.dateEdit.date()
+        start_date = temp.toPython().strftime('%b %d %Y %H:%M:%S')
+        return start_date
+
+
+    def getStop(self):
+        temp = self.ui.dateEdit_2.date()
+        end_date = temp.toPython().strftime('%b %d %Y %H:%M:%S')
+        return end_date
+
+
+    def screenData(self, line, last_date):
+        if 'Time:' in line:
+            ### Info for database.csv ###
+            # Get date and time
+            last_date = line.strip().split('Time: ', 1)[1].split(' PDT')[0]
+            last_date_stamp = datetime.strptime(last_date, '%a %b %d %Y %H:%M:%S')
+            last_date = last_date_stamp.strftime('%b %d %Y')
+            # Get license type
+            lic_type = (line.strip().split('Time:')[0].split(' ', 1)[1]).split(' ')[0]
+            return 'Time:', last_date_stamp, lic_type, last_date
+        elif 'OUT:' in line:
+            ### Info for database.csv ###
+            # Get time
+            last_out = datetime.strptime(line.strip().split('OUT: ')[0].split(' ')[0], '%H:%M:%S').time()
+            # Get date
+            date_out = datetime.strptime(last_date, '%b %d %Y').date()
+            last_out_stamp = datetime.combine(date_out, last_out)
+            # Get license type
+            lic_type = (line.strip().split('OUT:')[0].split(' ', 1)[1]).split(' ')[0]
+            return 'OUT:', last_out_stamp, lic_type, last_date
+        elif 'IN:' in line:
+            ### Info for database.csv ###
+            # Get time
+            last_in = datetime.strptime(line.strip().split('IN: ')[0].split(' ')[0], '%H:%M:%S').time()
+            # Get date
+            date_in = datetime.strptime(last_date, '%b %d %Y').date()
+            last_in_stamp = datetime.combine(date_in, last_in)
+            # Get license type
+            lic_type = (line.strip().split('IN:')[0].split(' ', 1)[1]).split(' ')[0]
+            return 'IN:', last_in_stamp, lic_type, last_date
+        else:
+            return 'None', 'None', 'None', last_date
+
+
+    def getFileDir(self):
+        return None
+
+    def lineParser(self, Lines):
+
+        lic_type = ""
+        last_date = datetime.now().strftime('%b %d %Y')
+        last_out_stamp = datetime.now().strftime('%b %d %Y')
+        last_in_stamp = datetime.now().strftime('%b %d %Y')
+        lic_inst = []
+        for line in Lines:
+            data_kind, data, lic, last_date = self.screenData(line, last_date)
+            if data_kind == 'Time:':
+                lic_type = lic
+            elif data_kind == 'OUT:':
+                last_out_stamp = data
+            elif data_kind == 'IN:':
+                last_in_stamp = data
+                ######################
+                # Get total time out
+                delta_time = self.timeDif(last_out_stamp, last_in_stamp)
+                lic_inst.append([lic_type, last_out_stamp.strftime('%a %b %d %Y %H:%M:%S'), last_in_stamp.strftime('%a %b %d %Y %H:%M:%S'), delta_time])
+        return lic_inst
+
+    def tableBuilder(self):
+        filepath = QFileDialog.getOpenFileName(self, caption='Open file', dir='.')[0]
+        file = open(filepath)
+        Lines = file.readlines()
+        file.close()
+        lic_inst = self.lineParser(Lines)
+
+        df_db = pd.DataFrame(lic_inst, columns=['License Type', 'Start Time', 'End Time', 'Delta Time'])
+        if self.ui.radioButton.isChecked():
+            df_db.to_csv(datetime.now().strftime('%H_%M_%S') + '_database.csv', sep='\t', encoding='utf-8', index=False)
+        df_db['Start Time'] = pd.to_datetime(df_db['Start Time'])
+        df_db['End Time'] = pd.to_datetime(df_db['End Time'])
+        start_date = self.getStart() # datetime.strptime(self.getStart(), '%b %d %Y %H:%M:%S').strftime('%Y-%m-%d %H:%M:%S')
+        end_date = self.getStop() # datetime.strptime(self.getStop(), '%b %d %Y %H:%M:%S').strftime('%Y-%m-%d %H:%M:%S')
+
+        mask = (df_db['Start Time'] >= start_date)
+        df_sel = df_db.loc[mask]
+        mask = (df_sel['End Time'] <= end_date)
+        df_pres = df_sel.loc[mask]
+        if self.ui.radioButton_2.isChecked():
+            df_sel.to_csv(datetime.now().strftime('%H_%M_%S') + '__from_' + str(start_date) + '_until_' + str(end_date)
+                          + '_database.csv', sep='\t', encoding='utf-8', index=False)
+
+        #self.ui.tableWidget.setModel(QTableWidgetItem(df_pres))
+        #self.ui.tableWidget.setItem(QTableWidgetItem(lic_inst))
+        df_pres = df_pres.reset_index()
+        r = 0
+        for index, row in df_pres.iterrows():
+            rose = self.ui.tableWidget.rowCount()
+            self.ui.tableWidget.insertRow(rose)
+            print(str(row))
+            print(str(index))
+            self.ui.tableWidget.setItem(rose, 0, QTableWidgetItem(str(row['License Type'])))
+            self.ui.tableWidget.setItem(rose, 1, QTableWidgetItem(str(row['Start Time'])))
+            self.ui.tableWidget.setItem(rose, 2, QTableWidgetItem(str(row['End Time'])))
+            self.ui.tableWidget.setItem(rose, 3, QTableWidgetItem(str(row['Delta Time'])))
+            r += 1
+        return None
+
+
+def main():
+    app = QApplication(sys.argv)
+    widget = MainWindow()
+    widget.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
---
+++ ui_parserGUI_3.py
@@ -0,0 +1,280 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'form_1.ui'
+##
+## Created by: Qt User Interface Compiler version 6.3.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QDateEdit, QFrame, QGridLayout,
+    QHBoxLayout, QHeaderView, QLabel, QMainWindow,
+    QPushButton, QRadioButton, QSizePolicy, QTabWidget,
+    QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.tabWidget = QTabWidget(self.centralwidget)
+        self.tabWidget.setObjectName(u"tabWidget")
+        self.tab = QWidget()
+        self.tab.setObjectName(u"tab")
+        self.verticalLayout_2 = QVBoxLayout(self.tab)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.frame_2 = QFrame(self.tab)
+        self.frame_2.setObjectName(u"frame_2")
+        self.frame_2.setFrameShape(QFrame.NoFrame)
+        self.frame_2.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_3 = QVBoxLayout(self.frame_2)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.frame_8 = QFrame(self.frame_2)
+        self.frame_8.setObjectName(u"frame_8")
+        self.frame_8.setFrameShape(QFrame.NoFrame)
+        self.frame_8.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_6 = QHBoxLayout(self.frame_8)
+        self.horizontalLayout_6.setObjectName(u"horizontalLayout_6")
+        self.logo = QFrame(self.frame_8)
+        self.logo.setObjectName(u"logo")
+        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(74)
+        sizePolicy.setVerticalStretch(74)
+        sizePolicy.setHeightForWidth(self.logo.sizePolicy().hasHeightForWidth())
+        self.logo.setSizePolicy(sizePolicy)
+        self.logo.setMinimumSize(QSize(0, 0))
+        self.logo.setMaximumSize(QSize(78, 59))
+        self.logo.setStyleSheet(u"QFrame{\n"
+"	border: 0px;\n"
+"	border-image: url( \"/Users/matt/Dropbox/QS_LogParser/logo.png\") 0 0 0 0 stretch stretch;\n"
+"	border-width: 0px;\n"
+"}")
+        self.logo.setFrameShape(QFrame.NoFrame)
+        self.logo.setFrameShadow(QFrame.Raised)
+
+        self.horizontalLayout_6.addWidget(self.logo)
+
+        self.label_3 = QLabel(self.frame_8)
+        self.label_3.setObjectName(u"label_3")
+        font = QFont()
+        font.setPointSize(30)
+        font.setBold(True)
+        self.label_3.setFont(font)
+        self.label_3.setLineWidth(100)
+        self.label_3.setAlignment(Qt.AlignCenter)
+
+        self.horizontalLayout_6.addWidget(self.label_3)
+
+
+        self.verticalLayout_3.addWidget(self.frame_8)
+
+
+        self.verticalLayout_2.addWidget(self.frame_2)
+
+        self.frame = QFrame(self.tab)
+        self.frame.setObjectName(u"frame")
+        self.frame.setFrameShape(QFrame.NoFrame)
+        self.frame.setFrameShadow(QFrame.Raised)
+        self.gridLayout = QGridLayout(self.frame)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.frame_6 = QFrame(self.frame)
+        self.frame_6.setObjectName(u"frame_6")
+        self.frame_6.setFrameShape(QFrame.NoFrame)
+        self.frame_6.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_4 = QHBoxLayout(self.frame_6)
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+        self.frame_4 = QFrame(self.frame_6)
+        self.frame_4.setObjectName(u"frame_4")
+        self.frame_4.setFrameShape(QFrame.NoFrame)
+        self.frame_4.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_3 = QHBoxLayout(self.frame_4)
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.label = QLabel(self.frame_4)
+        self.label.setObjectName(u"label")
+        font1 = QFont()
+        font1.setPointSize(13)
+        font1.setBold(False)
+        self.label.setFont(font1)
+
+        self.horizontalLayout_3.addWidget(self.label)
+
+        self.dateEdit = QDateEdit(self.frame_4)
+        self.dateEdit.setObjectName(u"dateEdit")
+
+        self.horizontalLayout_3.addWidget(self.dateEdit)
+
+
+        self.horizontalLayout_4.addWidget(self.frame_4)
+
+        self.frame_5 = QFrame(self.frame_6)
+        self.frame_5.setObjectName(u"frame_5")
+        self.frame_5.setFrameShape(QFrame.NoFrame)
+        self.frame_5.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_2 = QHBoxLayout(self.frame_5)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.label_2 = QLabel(self.frame_5)
+        self.label_2.setObjectName(u"label_2")
+
+        self.horizontalLayout_2.addWidget(self.label_2)
+
+        self.dateEdit_2 = QDateEdit(self.frame_5)
+        self.dateEdit_2.setObjectName(u"dateEdit_2")
+
+        self.horizontalLayout_2.addWidget(self.dateEdit_2)
+
+
+        self.horizontalLayout_4.addWidget(self.frame_5)
+
+
+        self.gridLayout.addWidget(self.frame_6, 0, 0, 1, 2)
+
+        self.frame_7 = QFrame(self.frame)
+        self.frame_7.setObjectName(u"frame_7")
+        self.frame_7.setFrameShape(QFrame.NoFrame)
+        self.frame_7.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_5 = QHBoxLayout(self.frame_7)
+        self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
+        self.frame_14 = QFrame(self.frame_7)
+        self.frame_14.setObjectName(u"frame_14")
+        self.frame_14.setFrameShape(QFrame.NoFrame)
+        self.frame_14.setFrameShadow(QFrame.Raised)
+        self.gridLayout_4 = QGridLayout(self.frame_14)
+        self.gridLayout_4.setObjectName(u"gridLayout_4")
+        self.radioButton = QRadioButton(self.frame_14)
+        self.radioButton.setObjectName(u"radioButton")
+
+        self.gridLayout_4.addWidget(self.radioButton, 0, 0, 1, 1)
+
+
+        self.horizontalLayout_5.addWidget(self.frame_14)
+
+        self.frame_13 = QFrame(self.frame_7)
+        self.frame_13.setObjectName(u"frame_13")
+        self.frame_13.setFrameShape(QFrame.NoFrame)
+        self.frame_13.setFrameShadow(QFrame.Raised)
+        self.gridLayout_3 = QGridLayout(self.frame_13)
+        self.gridLayout_3.setObjectName(u"gridLayout_3")
+        self.radioButton_2 = QRadioButton(self.frame_13)
+        self.radioButton_2.setObjectName(u"radioButton_2")
+
+        self.gridLayout_3.addWidget(self.radioButton_2, 0, 0, 1, 1)
+
+
+        self.horizontalLayout_5.addWidget(self.frame_13)
+
+
+        self.gridLayout.addWidget(self.frame_7, 1, 0, 1, 2)
+
+
+        self.verticalLayout_2.addWidget(self.frame)
+
+        self.frame_3 = QFrame(self.tab)
+        self.frame_3.setObjectName(u"frame_3")
+        self.frame_3.setFrameShape(QFrame.NoFrame)
+        self.frame_3.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout = QHBoxLayout(self.frame_3)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.frame_12 = QFrame(self.frame_3)
+        self.frame_12.setObjectName(u"frame_12")
+        self.frame_12.setFrameShape(QFrame.NoFrame)
+        self.frame_12.setFrameShadow(QFrame.Raised)
+
+        self.horizontalLayout.addWidget(self.frame_12)
+
+        self.frame_11 = QFrame(self.frame_3)
+        self.frame_11.setObjectName(u"frame_11")
+        self.frame_11.setFrameShape(QFrame.NoFrame)
+        self.frame_11.setFrameShadow(QFrame.Raised)
+        self.gridLayout_2 = QGridLayout(self.frame_11)
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.pushButton = QPushButton(self.frame_11)
+        self.pushButton.setObjectName(u"pushButton")
+
+        self.gridLayout_2.addWidget(self.pushButton, 0, 0, 1, 1)
+
+
+        self.horizontalLayout.addWidget(self.frame_11)
+
+        self.frame_10 = QFrame(self.frame_3)
+        self.frame_10.setObjectName(u"frame_10")
+        self.frame_10.setFrameShape(QFrame.NoFrame)
+        self.frame_10.setFrameShadow(QFrame.Raised)
+
+        self.horizontalLayout.addWidget(self.frame_10)
+
+
+        self.verticalLayout_2.addWidget(self.frame_3)
+
+        self.tabWidget.addTab(self.tab, "")
+        self.tab_2 = QWidget()
+        self.tab_2.setObjectName(u"tab_2")
+        self.tableWidget = QTableWidget(self.tab_2)
+        if (self.tableWidget.columnCount() < 4):
+            self.tableWidget.setColumnCount(4)
+        __qtablewidgetitem = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(0, __qtablewidgetitem)
+        __qtablewidgetitem1 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(1, __qtablewidgetitem1)
+        __qtablewidgetitem2 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(2, __qtablewidgetitem2)
+        __qtablewidgetitem3 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(3, __qtablewidgetitem3)
+        self.tableWidget.setObjectName(u"tableWidget")
+        self.tableWidget.setGeometry(QRect(10, 10, 751, 531))
+        self.tableWidget.setAlternatingRowColors(True)
+        self.tableWidget.setColumnCount(4)
+        self.tableWidget.horizontalHeader().setCascadingSectionResizes(False)
+        self.tableWidget.horizontalHeader().setMinimumSectionSize(19)
+        self.tableWidget.horizontalHeader().setDefaultSectionSize(190)
+        self.tableWidget.horizontalHeader().setProperty("showSortIndicator", False)
+        self.tableWidget.horizontalHeader().setStretchLastSection(False)
+        self.tableWidget.verticalHeader().setCascadingSectionResizes(False)
+        self.tableWidget.verticalHeader().setDefaultSectionSize(30)
+        self.tableWidget.verticalHeader().setProperty("showSortIndicator", False)
+        self.tableWidget.verticalHeader().setStretchLastSection(False)
+        self.tabWidget.addTab(self.tab_2, "")
+
+        self.verticalLayout.addWidget(self.tabWidget)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+
+        self.tabWidget.setCurrentIndex(0)
+
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"QuickSilver Aerospace LLC Log File Parser", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"Start Date:", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"End Date:", None))
+        self.radioButton.setText(QCoreApplication.translate("MainWindow", u"Save .csv of all data", None))
+        self.radioButton_2.setText(QCoreApplication.translate("MainWindow", u"Save .csv of selected data", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"OK", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), QCoreApplication.translate("MainWindow", u"Setup", None))
+        ___qtablewidgetitem = self.tableWidget.horizontalHeaderItem(0)
+        ___qtablewidgetitem.setText(QCoreApplication.translate("MainWindow", u"License Type", None));
+        ___qtablewidgetitem1 = self.tableWidget.horizontalHeaderItem(1)
+        ___qtablewidgetitem1.setText(QCoreApplication.translate("MainWindow", u"Start Time", None));
+        ___qtablewidgetitem2 = self.tableWidget.horizontalHeaderItem(2)
+        ___qtablewidgetitem2.setText(QCoreApplication.translate("MainWindow", u"End Time", None));
+        ___qtablewidgetitem3 = self.tableWidget.horizontalHeaderItem(3)
+        ___qtablewidgetitem3.setText(QCoreApplication.translate("MainWindow", u"Delta Time", None));
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), QCoreApplication.translate("MainWindow", u"Table", None))
+    # retranslateUi
+
